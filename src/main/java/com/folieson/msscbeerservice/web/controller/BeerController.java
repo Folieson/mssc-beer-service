83ee3e301be6438d606511bc8b010be18780cc70
Andrey Ponamarchuk <andrey.ponamarchuk@x5.ru>
2021-09-02 19:03:33 +0500

    adding controller impl

--- src/main/java/com/folieson/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/folieson/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,9 @@
 package com.folieson.msscbeerservice.web.controller;
 
+import com.folieson.msscbeerservice.repositories.BeerRepository;
+import com.folieson.msscbeerservice.web.mappers.BeerMapper;
 import com.folieson.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,28 +15,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
+  private final BeerMapper beerMapper;
+  private final BeerRepository beerRepository;
 
   @GetMapping("/{beerId}")
   public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
-    //TODO: impl
-    return ResponseEntity.status(HttpStatus.OK).body(BeerDto.builder().build());
+    return beerRepository.findById(beerId)
+        .map(beer -> ResponseEntity.status(HttpStatus.OK).body(beerMapper.beerToBeerDto(beer)))
+        .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
   }
 
   @PostMapping
   public ResponseEntity<String> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
-    //TODO: impl
+    beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
   @PutMapping("/{beerId}")
   public ResponseEntity<String> updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto) {
-    //TODO: impl
+    beerRepository.findById(beerId).ifPresent(beer -> {
+      beer.setBeerName(beerDto.getBeerName());
+      beer.setBeerStyle(beerDto.getBeerStyle().name());
+      beer.setPrice(beerDto.getPrice());
+      beer.setUpc(beerDto.getUpc());
+
+      beerRepository.save(beer);
+    });
+
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 }
--- src/test/java/com/folieson/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/folieson/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,23 +1,31 @@
 package com.folieson.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.folieson.msscbeerservice.domain.Beer;
+import com.folieson.msscbeerservice.repositories.BeerRepository;
 import com.folieson.msscbeerservice.web.model.BeerDto;
 import com.folieson.msscbeerservice.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "com.folieson.msscbeerservice.web.mappers")
 class BeerControllerTest {
 
   private static final String URL = "/api/v1/beer/";
@@ -26,9 +34,12 @@ class BeerControllerTest {
   private MockMvc mockMvc;
   @Autowired
   private ObjectMapper objectMapper;
+  @MockBean
+  private BeerRepository beerRepository;
 
   @Test
   void getBeerById() throws Exception {
+    given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
     mockMvc.perform(get(URL + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk());
   }
